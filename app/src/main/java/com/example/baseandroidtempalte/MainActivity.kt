7bc64e72fa85e3cf4bbf5d6038f170e6c63b02e7
jatin <jatinjagia12@gmail.com>
2023-07-11 00:19:24 +0530

    added navigation component dependency

--- app/src/main/java/com/example/baseandroidtempalte/MainActivity.kt
+++ app/src/main/java/com/example/baseandroidtempalte/MainActivity.kt
@@ -2,12 +2,11 @@ package com.example.baseandroidtempalte
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.baseandroidtemplate.actions.Actions
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Actions.openHomeIntent(this))
+
     }
 }
