052698d1f50bb46b7bdd095944e6710886df9e9b
jatinjagia <jatinjagia12@gmail.com>
2025-04-16 18:37:21 +0530

    added kotlin kapt support to support data binding in latest AS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("androidx.navigation.safeargs.kotlin")
+    id("org.jetbrains.kotlin.kapt")
+
 }
 
 android {
